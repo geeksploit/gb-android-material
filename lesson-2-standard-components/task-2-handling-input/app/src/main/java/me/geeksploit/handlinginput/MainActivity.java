b2252f2a68f6d44b2b440a4ed545f889e3746558
geeksploit <geeksploit@shootan.com>
2018-02-28 17:44:13 +0300

    Add InputValidator class stub

--- lesson-2-standard-components/task-2-handling-input/app/src/main/java/me/geeksploit/handlinginput/MainActivity.java
+++ lesson-2-standard-components/task-2-handling-input/app/src/main/java/me/geeksploit/handlinginput/MainActivity.java
@@ -2,6 +2,8 @@ package me.geeksploit.handlinginput;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final class InputValidator implements TextView.OnEditorActionListener {
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            return false;
+        }
     }
 }
