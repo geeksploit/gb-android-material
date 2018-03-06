a40d4f5ce9e104283122fd45f02320d7aa54c7a0
geeksploit <geeksploit@shootan.com>
2018-03-06 03:30:20 +0300

    Replace multiple if statements with a single switch block

--- lesson-4-navigation-drawer/task-2-drawer-listener/app/src/main/java/me/geeksploit/navigationdrawertouchlistener/MainActivity.java
+++ lesson-4-navigation-drawer/task-2-drawer-listener/app/src/main/java/me/geeksploit/navigationdrawertouchlistener/MainActivity.java
@@ -83,20 +83,21 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch (item.getItemId()) {
+            case R.id.nav_camera:
+                break;
+            case R.id.nav_gallery:
+                break;
+            case R.id.nav_slideshow:
+                break;
+            case R.id.nav_manage:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_send:
+                break;
+            default:
+                throw new UnsupportedOperationException();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
